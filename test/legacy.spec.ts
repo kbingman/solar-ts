4744dfcda9fea03d0adcf0eb361a841adeb67d2b
Keith Bingman <kbingman@gmail.com>
2020-09-12 21:26:30 -0700

    fixing tests

--- test/legacy.spec.ts
+++ test/legacy.spec.ts
@@ -1,5 +1,4 @@
 import { 
-  nBodyProblem, 
   updatePositionVectors, 
   updateAccelerationVectors, 
   updateVelocityVectors,
@@ -11,74 +10,31 @@ const G = 39.5;
 const SOFTENING_CONSTANT = 0.15;
 const DT = 0.008; //0.005 years is equal to 1.825 days
 
-test.skip('nbody updatePositionVectors', () => {
-  const masses = JSON.parse(JSON.stringify(bodies));
-  const nbody = new nBodyProblem({ masses, g: G, dt: DT, softeningConstant: SOFTENING_CONSTANT });
-  const x = nbody.masses[3].x;
-  nbody.updatePositionVectors()
-
-  expect(x - nbody.masses[3].x).toBe(0.038806842004751374);
-});
-
-test.skip('nbody updateAccelerationVectors', () => {
-  const masses = JSON.parse(JSON.stringify(bodies));
-  const nbody = new nBodyProblem({ masses, g: G, dt: DT, softeningConstant: SOFTENING_CONSTANT });
-  const x = nbody.masses[3].ax || 0;
-  nbody.updateAccelerationVectors()
-
-  expect(x - nbody.masses[3].ax).toBe(24.595433864433154);
-});
-
-test.skip('nbody updateVelocityVectors', () => {
-  const masses = JSON.parse(JSON.stringify(bodies));
-  const nbody = new nBodyProblem({ masses, g: G, dt: DT, softeningConstant: SOFTENING_CONSTANT });
-  const x = nbody.masses[3].vx || 0;
-  nbody.updateVelocityVectors()
-
-  expect(x - nbody.masses[3].vx).toBe(0);
-});
-
-test.skip('nbody', () => {
-  const masses = JSON.parse(JSON.stringify(bodies));
-  const nbody = new nBodyProblem({ masses, g: G, dt: DT, softeningConstant: SOFTENING_CONSTANT });
-  const x = nbody.masses[3].x || 0;
-  console.time('nbody');
-  nbody
-    .updatePositionVectors()
-    .updateAccelerationVectors()
-    .updateVelocityVectors();
-  console.timeEnd('nbody');
-
-  expect(x - nbody.masses[3].x).toBe(0.038806842004751374);
-});
-
-test.skip('functional', () => {
+test('functional', () => {
   const masses = JSON.parse(JSON.stringify(bodies));
   const x = masses[3].x || 0;
-  console.time('functional');
   updateBodies(G, SOFTENING_CONSTANT, DT)(masses);
-  console.timeEnd('functional');
 
-  expect(x - masses[3].x).toBe(0.038806842004751374);
+  expect(x - masses[3].x).toBe(-0.00013331230969529373);
 });
 
-test.skip('updatePositionVectors', () => {
+test('updatePositionVectors', () => {
   const masses = JSON.parse(JSON.stringify(bodies));
   const x = masses[3].x;
   updatePositionVectors(DT, masses)
 
-  expect(x - masses[3].x).toBe(0.038806842004751374);
+  expect(x - masses[3].x).toBe(-0.00013331230969529373);
 });
 
-test.skip('updateAccelerationVectors', () => {
+test('updateAccelerationVectors', () => {
   const masses = JSON.parse(JSON.stringify(bodies));
   const x = masses[3].ax || 0;
   updateAccelerationVectors(G, SOFTENING_CONSTANT, masses)
 
-  expect(x - masses[3].ax).toBe(24.595433864433154);
+  expect(x - masses[3].ax).toBe(-6.055247336454543);
 });
 
-test.skip('updateVelocityVectors', () => {
+test('updateVelocityVectors', () => {
   const masses = JSON.parse(JSON.stringify(bodies));
   const x = masses[3].vx || 0;
   updateVelocityVectors(DT, bodies)
