4146875a1a5b88a4916db74da3a423e0d62b8afd
Keith Bingman <keith@keithbingman.com>
2024-05-06 21:55:49 -0700

    Tests
    Updating new tests and removing legacy code and tests.

--- src/draw.spec.tsx
+++ src/draw.spec.tsx
@@ -1,8 +1,8 @@
-import { test, expect, vi } from "vitest";
-import { drawRect, drawCircle, fillCanvas } from "../src/draw";
+import { test, expect, vi } from 'vitest';
+import { drawRect, drawCircle, fillCanvas } from './draw';
 
 const createMockCtx = () =>
-  (({
+  ({
     arc: vi.fn(),
     beginPath: vi.fn(),
     canvas: {
@@ -11,32 +11,32 @@ const createMockCtx = () =>
     },
     fill: vi.fn(),
     fillRect: vi.fn(),
-  } as unknown) as CanvasRenderingContext2D);
+  }) as unknown as CanvasRenderingContext2D;
 
-test("drawRect", () => {
+test('drawRect', () => {
   const ctx = createMockCtx();
-  drawRect(ctx, 0, 0, 24, 24, "chartreuse");
+  drawRect(ctx, 0, 0, 24, 24, 'chartreuse');
 
   expect(ctx.fillRect).toHaveBeenCalledWith(0, 0, 24, 24);
-  expect(ctx.fillStyle).toBe("chartreuse");
+  expect(ctx.fillStyle).toBe('chartreuse');
   expect(ctx.fill).toHaveBeenCalled();
 });
 
-test("drawCircle", () => {
+test('drawCircle', () => {
   const ctx = createMockCtx();
-  drawCircle(ctx, 32, 32, 16, "chartreuse");
+  drawCircle(ctx, 32, 32, 16, 'chartreuse');
 
   expect(ctx.beginPath).toHaveBeenCalled();
   expect(ctx.arc).toHaveBeenCalledWith(32, 32, 16, 0, Math.PI * 2, false);
-  expect(ctx.fillStyle).toBe("chartreuse");
+  expect(ctx.fillStyle).toBe('chartreuse');
   expect(ctx.fill).toHaveBeenCalled();
 });
 
-test("fillCanvas", () => {
+test('fillCanvas', () => {
   const ctx = createMockCtx();
-  fillCanvas(ctx, "lightcoral");
+  fillCanvas(ctx, 'lightcoral');
 
   expect(ctx.fillRect).toHaveBeenCalledWith(0, 0, 32, 48);
-  expect(ctx.fillStyle).toBe("lightcoral");
+  expect(ctx.fillStyle).toBe('lightcoral');
   expect(ctx.fill).toHaveBeenCalled();
 });
--- src/physics.test.ts
+++ src/physics.test.ts
@@ -11,7 +11,7 @@ const G = 39.5;
 const SOFTENING_CONSTANT = 0.15;
 
 describe('physics', () => {
-  test.skip('updatePositionVectors', () => {
+  test('updatePositionVectors', () => {
     expect(updatePositionVectors(1, planets)[0]).toEqual({
       name: 'Sun',
       color: 'yellow',
@@ -35,14 +35,14 @@ describe('physics', () => {
       color: 'yellow',
       m: 1,
       position: [
-        -0.005129093417839196, 0.006987018492991927, 0.00006034205790824802,
+        -0.005137171274636994, 0.006983306435237695, 0.0000605746545589308,
       ],
       velocity: [
         -0.000008077856797797696, -0.000003712057754231977,
         2.325966506827774e-7,
       ],
       accelleration: [
-        -0.000018177005128062863, -0.00010593081119597778, 5.214104876603709e-9,
+        0.00048212324221185153, -0.001716377855023952, -0.000006971793438640271,
       ],
     });
   });
